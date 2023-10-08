936b86853ad10948f451ab24334641ac25c325d3
FNAl3 <147249118+FNAl3@users.noreply.github.com>
2023-10-08 18:01:12 +0200

    Add files via upload

--- Creacion de usuarios de forma masiva.ps1
+++ Creacion de usuarios de forma masiva.ps1
@@ -2,6 +2,6 @@ $usuarios=Import-Csv -Path c:\...\ficehro con los datos
 foreach  ($i in $usuarios)
 	{
 		$clave=ConvertTo-SecureString $i.contra -AsPlainText -Force
-		New-LocalUser $i.nombre -Password $clave -AccountNeverExpires -PassNeverExpires
+		New-LocalUser $i.nombre -Password $clave -AccountNeverExpires -PasswordNeverExpires
 		Add-LocalGroupMember -Group usuarios -Member $i.nombre
 	}
